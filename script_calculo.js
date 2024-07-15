0c8bc2d132e9dbf2cdb77c7e9c8d9f3a613a8062
luanacostav <costavasconcelosluana@gmail.com>
2024-07-15 20:07:31 -0300

    first commit

---
+++ script_calculo.js
@@ -0,0 +1,45 @@
+// ________________________ Soma ________________________
+
+function soma(a, b){
+    return a + b;
+}
+
+console.log("Soma =",soma(4,5));
+console.log("Soma =",soma(2,3));
+
+// ________________________ Subtração ________________________
+
+function subtracao(f, h){
+    return f - h;
+}
+
+console.log("Subtração =",subtracao(7,5));
+console.log("Subtração =",subtracao(10,5));
+
+// ________________________ Multiplicação ________________________
+
+function mult(x, y = 2){
+    return x * y;
+}
+
+console.log("Multiplicação =",mult(5));
+console.log("Multiplicação =",mult(5,3));
+
+//________________________ Divisão ________________________
+
+function divisao(d,c){
+    return d / c;
+}
+
+console.log("Divisão =",divisao(10,5));
+console.log("Divisão =",divisao(3,3));
+
+// ________________________ Potenciação ________________________
+
+function potencia(b,e){
+    return b ** e;
+}
+
+console.log("Potenciação =",potencia(2,2));
+console.log("Potenciação =",potencia(4,2));
+
